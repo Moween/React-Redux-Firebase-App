159df346e779f0ccec73289561215d181f8ae996
moween <moween1998@gmail.com>
2021-12-20 23:54:50 +0100

    Add not found page

---
+++ react-redux-firebase-app/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Box';
+
+const NotFound = () => {
+  return (
+    <Box>
+     <Typography>Page Not Found</Typography> 
+    </Box>
+  )
+}
+
+export default NotFound
