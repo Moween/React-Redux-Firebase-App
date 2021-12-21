ad09596397e92e51ef7da83009fd735c589bfad9
moween <moween1998@gmail.com>
2021-12-21 12:15:21 +0100

    Rename file

--- react-redux-firebase-app/src/services/firebase/firebase.config.js
+++ react-redux-firebase-app/src/services/firebase/firebase.config.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
+import firebase   from 'firebase/compat/app';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +23,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+firebase.firestore();
