a16ed0dd06ce006f4946c49e13ec974a021dfacb
moween <moween1998@gmail.com>
2022-01-01 10:56:57 +0100

    Add footer component

---
+++ react-redux-firebase-app/src/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+const Footer = () => {
+  return (
+    <Box component="footer">
+      <Container maxWidth="xl">
+      </Container>
+    </Box>
+  )
+}
+
+export default Footer
