81e1bc9730e843a4b8a4f19ce6489f45b66bba5d
moween <moween1998@gmail.com>
2022-01-01 10:55:28 +0100

    Use react-redux-firebase

--- react-redux-firebase-app/src/index.js
+++ react-redux-firebase-app/src/index.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import firebase   from 'firebase/compat/app';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { getAnalytics } from "firebase/analytics";
+import { createFirestoreInstance } from 'redux-firestore';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import App from './App';
 import './index.css';
+import { firebaseConfig } from './services/firebase/firebase.config';
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+firebase.firestore();
+
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true,
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance, //since we are using Firestore
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
