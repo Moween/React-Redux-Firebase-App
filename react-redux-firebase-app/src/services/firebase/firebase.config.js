10bcd10d4bd1c99dc52753457bf9b87037da3d5f
moween <moween1998@gmail.com>
2022-01-01 10:56:26 +0100

    Remove imports and comments

--- react-redux-firebase-app/src/services/firebase/firebase.config.js
+++ react-redux-firebase-app/src/services/firebase/firebase.config.js
@@ -1,26 +1,10 @@
-// Import the functions you need from the SDKs you need
-import firebase   from 'firebase/compat/app';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD4saxEnYaAkHUsaxsWRFPwIpCYSXxyCkA",
-  authDomain: "events-72a59.firebaseapp.com",
-  projectId: "events-72a59",
-  storageBucket: "events-72a59.appspot.com",
-  messagingSenderId: "203091800149",
-  appId: "1:203091800149:web:915a00c320dd3e37c224dc",
-  measurementId: "G-J5S0TTRH15"
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-firebase.firestore();
