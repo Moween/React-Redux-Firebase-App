dd1232279d8e29aa02f6e550f96fd5a011e18afa
moween <moween1998@gmail.com>
2022-01-08 22:25:45 +0100

    Use auth state to render template conditionally.

--- react-redux-firebase-app/src/pages/Home.jsx
+++ react-redux-firebase-app/src/pages/Home.jsx
@@ -5,36 +5,48 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import { useSelector } from 'react-redux';
 
-import eventImg  from '../assets/home-event-img.jpg';
-
-
+import eventImg from '../assets/home-event-img.jpg';
+import DashBoard from '../components/dashboard/DashBoard';
 const Home = () => {
   const mobile = useSelector((state) => state.mediaQuery.smallScreen);
+  const { isLoaded, isEmpty } = useSelector((state) => state.firebase.auth);
   return (
     <Box>
       <Container>
-        <Box component="section" className="md:flex  pt-4 items-center">
-          <Box className="p-1">
-            <Typography className="p-1" variant={mobile ? 'h5' : 'h3'} component="h2">
-              Make your events public and keep people talking about it.
-            </Typography>
-            <Typography sx={{ p: '0.5rem' }} component="p">
-              Event sharing software that makes event sharing easy. 
-              Invite people to your events.
-            </Typography>
-            <Box>
-              <Button variant="contained" href='/registeruser'>Join for free</Button>
-              <Button variant="outlined" href="/loginuser">Sign In</Button>
+        {isLoaded && isEmpty ? (
+          <Box component="section" className="md:flex  pt-4 items-center">
+            <Box className="p-1">
+              <Typography
+                className="p-1"
+                variant={mobile ? 'h5' : 'h3'}
+                component="h2"
+              >
+                Make your events public and keep people talking about it.
+              </Typography>
+              <Typography sx={{ p: '0.5rem' }} component="p">
+                Event sharing software that makes event sharing easy. Invite
+                people to your events.
+              </Typography>
+              <Box>
+                <Button variant="contained" href="/registeruser">
+                  Join for free
+                </Button>
+                <Button variant="outlined" href="/loginuser">
+                  Sign In
+                </Button>
+              </Box>
+            </Box>
+            <Box className="md:w-3/4">
+              <img
+                className="w-full"
+                src={eventImg}
+                alt="A black boy with system"
+              />
             </Box>
           </Box>
-          <Box className="md:w-3/4">
-            <img
-              className="w-full"
-              src={eventImg}
-              alt="A black boy with system"
-            />
-          </Box>
-        </Box>
+        ) : (
+          <DashBoard />
+        )}
       </Container>
     </Box>
   );
