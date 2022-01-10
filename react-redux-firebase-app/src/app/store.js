0a6229c8ea85b97342f0c3ffde78102d9722e05e
moween <moween1998@gmail.com>
2022-01-10 13:27:48 +0100

    Remove media-query slice

--- react-redux-firebase-app/src/app/store.js
+++ react-redux-firebase-app/src/app/store.js
@@ -1,12 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-import mediaQuerySlice from './reducers/mediaQuerySlice';
-import authSlice from './reducers/authSlice';
-import createPostSlice from './reducers/createPostSlice'
+import authSlice from '../reducers/authSlice';
+import createPostSlice from '../reducers/createPostSlice';
+import {
+  firebaseReducer,
+  actionTypes,
+} from 'react-redux-firebase';
+import { firestoreReducer } from 'redux-firestore';
 
 export const store = configureStore({
   reducer: {
-    mediaQuery: mediaQuerySlice,
     auth: authSlice,
     createPost: createPostSlice,
+    firebase: firebaseReducer,
+    firestore: firestoreReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [actionTypes.LOGIN, actionTypes.AUTH_LINK_ERROR],
+      },
+    }),
 });
