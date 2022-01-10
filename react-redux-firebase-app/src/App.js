7c31336ef279cecf1cbdf9a01adc792dccfbe40d
moween <moween1998@gmail.com>
2022-01-10 13:36:46 +0100

    Delete mediaQuery  actions and stop side effects

--- react-redux-firebase-app/src/App.js
+++ react-redux-firebase-app/src/App.js
@@ -1,46 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
-import { useMediaQuery } from '@mui/material';
-import { useDispatch } from 'react-redux';
-
 
 import Header from './layout/Header';
 import Home from './pages/Home';
 import LoginUser from './pages/LoginUser';
 import Main from './layout/Main';
 import NotFound from './pages/NotFound';
+import PrivateRoute from './components/PrivateRoute';
 import RegisterUser from './pages/RegisterUser';
 import { theme } from './utils/MuiStyles';
-import {
-  setMediumScreen,
-  setLargeScreen,
-  setSmallScreen,
-} from './reducers/mediaQuerySlice';
-import './App.css';
-import PrivateRoute from './components/PrivateRoute';
 import { navLinks } from './utils/data';
+import './App.css';
 
 function App() {
-  const dispatch = useDispatch();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-  const isTablet = useMediaQuery(theme.breakpoints.down('lg'));
-  const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
-
-  useEffect(() => {
-    dispatch(setSmallScreen(isMobile));
-    // eslint-disable-next-line
-  }, []);
-
-  useEffect(() => {
-    dispatch(setMediumScreen(isTablet));
-    // eslint-disable-next-line
-  }, []);
-
-  useEffect(() => {
-    dispatch(setLargeScreen(isDesktop));
-    // eslint-disable-next-line
-  }, []);
+  // const isTablet = useMediaQuery(theme.breakpoints.between('sm', "md"));
+  // const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
     <ThemeProvider theme={theme}>
