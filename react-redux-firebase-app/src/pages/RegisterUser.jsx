792ddcef9b04b45bafd17e5ed11a1ab84e0eaee2
moween <moween1998@gmail.com>
2022-01-11 10:02:08 +0100

    Add create new user function

--- react-redux-firebase-app/src/pages/RegisterUser.jsx
+++ react-redux-firebase-app/src/pages/RegisterUser.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import Registration from '../utils/registration';
+import Registration from '../utils/registerUser';
 import { useDispatch } from 'react-redux';
+import { useFirebase } from 'react-redux-firebase';
 
 const RegisterUser = () => {
   const dispatch = useDispatch();
-  return <Registration dispatch={dispatch} />;
+  const firebase = useFirebase();
+
+  const createNewUser = async ({ email, password, username }) => {
+    await firebase.createUser({ email, password }, { username, email });
+  };
+
+  return <Registration  dispatch={dispatch} registerUser={createNewUser}/>;
 };
 
 export default RegisterUser;
