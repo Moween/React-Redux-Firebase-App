3889403a0eeebb01f734f97bbb9f6f7a40931d24
moween <moween1998@gmail.com>
2022-01-14 09:26:58 +0100

    Remove auth slice from store

--- react-redux-firebase-app/src/app/store.js
+++ react-redux-firebase-app/src/app/store.js
@@ -1,23 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authSlice from '../reducers/authSlice';
 import createPostSlice from '../reducers/createPostSlice';
 import {
   firebaseReducer,
-  actionTypes,
+  
 } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice,
     createPost: createPostSlice,
     firebase: firebaseReducer,
     firestore: firestoreReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [actionTypes.LOGIN, actionTypes.AUTH_LINK_ERROR],
-      },
+      serializableCheck: false,
     }),
 });
