16ce49d2831f6c3f7c80b5d1d67e40116c29b94e
moween <moween1998@gmail.com>
2022-01-16 21:37:18 +0100

    Remove link to register form

--- react-redux-firebase-app/src/layout/form/LoginForm.jsx
+++ react-redux-firebase-app/src/layout/form/LoginForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 import InputField from './TextField';
@@ -9,62 +8,44 @@ import InputField from './TextField';
 const Login = ({ values, touched, errors, handleChange, handleSubmit }) => {
   return (
     <Box
+      component="form"
+      id="login-form"
       sx={{
+        mb: '2rem',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '70vh',
+        flexDirection: 'column',
       }}
-      className=""
+      onSubmit={handleSubmit}
+      autoComplete="off"
     >
-      <Box
-        component="form"
-        id="login-form"
-        sx={{
-          width: { xs: '70%', lg: '30%' },
-          m: '0 auto',
-          // mt: '2rem',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-        onSubmit={handleSubmit}
-        autoComplete="off"
+      <InputField
+        name="email"
+        label="Email"
+        type="email"
+        value={values.email}
+        onChange={handleChange}
+        error={touched.email && Boolean(errors.email)}
+        helperText={touched.email && errors.email}
+      />
+
+      <InputField
+        label="Password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+        type="password"
+        error={touched.password && Boolean(errors.password)}
+        helperText={touched.password && errors.password}
+      />
+
+      <Button
+        type="submit"
+        variant="contained"
+        endIcon={<NavigateNextIcon />}
+        disableRipple={true}
       >
-        <InputField
-          name="email"
-          label="Email"
-          type="email"
-          value={values.email}
-          onChange={handleChange}
-          error={touched.email && Boolean(errors.email)}
-          helperText={touched.email && errors.email}
-        />
-
-        <InputField
-          label="Password"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-          type="password"
-          error={touched.password && Boolean(errors.password)}
-          helperText={touched.password && errors.password}
-        />
-
-        <Button
-          type="submit"
-          variant="contained"
-          endIcon={<NavigateNextIcon />}
-          disableRipple={true}
-        >
-          Login
-        </Button>
-        <Link
-          sx={{ m: '0 auto', mt: '2rem', textDecoration: 'none' }}
-          href="/registeruser"
-        >
-          Don't have an account? Register
-        </Link>
-      </Box>
+        Login
+      </Button>
     </Box>
   );
 };
