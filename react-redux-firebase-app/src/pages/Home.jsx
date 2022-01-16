a6c5d823b6719c1615a9f33dd746d6c6133bdb33
moween <moween1998@gmail.com>
2022-01-16 22:36:39 +0100

    Landing page template  and styles

--- react-redux-firebase-app/src/pages/Home.jsx
+++ react-redux-firebase-app/src/pages/Home.jsx
@@ -13,47 +13,58 @@ import { theme } from '../utils/MuiStyles';
 
 const Home = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isTablet = useMediaQuery(theme.breakpoints.between('sm', "md"));
+
   const { isLoaded, isEmpty } = useSelector((state) => state.firebase.auth);
 
   const displayHomeContent = () => {
     if (isLoaded && isEmpty) {
       return (
         <Box
-          component="section"
-          className="flex flex-col-reverse  md:flex-row  pt-4 items-center"
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '80vh',
+          }}
         >
-          <Box className="p-1 pl-0">
-            <Typography
-              className="p-1 pl-0"
-              variant={isMobile ? 'h5' : 'h4'}
-              component="h1"
-            >
-              Make your events public and keep people talking about it.
-            </Typography>
-            <Typography component="p" className="pl-0 p-4">
-              Event sharing software that makes event sharing easy. Invite
-              people to your events.
-            </Typography>
-            <Stack
-              direction={{ xs: 'column', sm: 'row' }}
-              aria-label="vertical contained button group"
-              size="large"
-              spacing={{ xs: 1, md: 2 }}
-            >
-              <Button variant="contained" href="/registeruser">
-                Join for free
-              </Button>
-              <Button variant="outlined" href="/loginuser">
-                Sign In
-              </Button>
-            </Stack>
-          </Box>
-          <Box className="md:w-3/4">
-            <img
-              className="w-full"
-              src={eventImg}
-              alt="A black boy with system"
-            />
+          <Box
+            component="section"
+            className="flex flex-col-reverse  lg:flex-row lg:pt-4 items-center"
+          >
+            <Box className="p-1 pl-0">
+              <Typography
+                className="p-1 pl-0"
+                variant={isMobile ? 'h5' : 'h3'}
+                component="h1"
+              >
+                Make your events public
+              </Typography>
+              <Typography component="p" className="pl-0 p-4">
+                Event sharing software that makes event sharing easy. Invite
+                people to your events and keep them talking about it.
+              </Typography>
+              <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                aria-label="vertical contained button group"
+                size="large"
+                spacing={{ xs: 1, md: 2 }}
+              >
+                <Button variant="contained" href="/registeruser">
+                  Join for free
+                </Button>
+                <Button variant="outlined" href="/loginuser">
+                  Sign In
+                </Button>
+              </Stack>
+            </Box>
+            <Box className="lg:w-3/4">
+              <img
+                className="w-full"
+                src={eventImg}
+                alt="A black boy with system"
+              />
+            </Box>
           </Box>
         </Box>
       );
@@ -63,6 +74,7 @@ const Home = () => {
   };
   return (
     <Box>
+      {/* <ToastContainer /> */}
       <Container>{displayHomeContent()}</Container>
     </Box>
   );
