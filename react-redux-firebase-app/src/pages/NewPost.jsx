f3ac18ee0e7275d03e48245c102f53f3b327390f
moween <moween1998@gmail.com>
2022-01-19 23:17:33 +0100

    Implement create post feature

--- react-redux-firebase-app/src/pages/NewPost.jsx
+++ react-redux-firebase-app/src/pages/NewPost.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import PostValidation from '../utils/createPost';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { toast } from 'react-toastify';
+import { useFirestore } from 'react-redux-firebase';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import CreatePost from '../layout/form/CreatePost';
 
 const NewPost = () => {
-  const dispatch = useDispatch();
-  return <PostValidation dispatch={dispatch} />;
-}
+  const firestore = useFirestore();
+  const navigate = useNavigate();
+  const { uid } = useSelector((state) => state.firebase.auth);
+  const { username } = useSelector((state) => state.firebase.profile);
+
+  const createPost = async (post) => {
+    try {
+      const timestamp = firestore.Timestamp.fromDate(new Date());
+      await firestore
+        .collection('events')
+        .doc(uid)
+        .collection('events')
+        .add({ ...post, createdAt: timestamp, postedBy: username });
+      toast.success('Post created');
+      navigate('/');
+    } catch (error) {
+      const errorMsg = error.message
+        .replace(')', '')
+        .replace(/-/g, ' ')
+        .split('/')
+        .pop();
+      toast.error(errorMsg);
+    }
+  };
+
+  return <CreatePost createPost={createPost} />;
+};
 
 export default NewPost;
