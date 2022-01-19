eb645e4248c52012b5532dee333b058ce1b00944
moween <moween1998@gmail.com>
2022-01-19 23:18:27 +0100

    Implement login feature

--- react-redux-firebase-app/src/pages/LoginUser.jsx
+++ react-redux-firebase-app/src/pages/LoginUser.jsx
@@ -1,17 +1,52 @@
 import React from 'react';
-import Login  from '../utils/loginUser';
-import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useFirebase } from 'react-redux-firebase';
+import { toast } from 'react-toastify';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
+import Login from '../layout/form/LoginUser';
 
 const LoginUser = () => {
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const firebase = useFirebase();
 
-  const loginUserWithEmail = async({ email, password }) => {
-    await firebase.login({ email, password })
-  }
+  const loginUserWithEmail = async ({ email, password }) => {
+    try {
+      await firebase.login({ email, password });
+      toast.success('You are signed in');
+      navigate('/', { replace: true });
+    } catch (error) {
+      const errorMsg = error.message
+      .replace(')', '')
+      .replace(/-/g, ' ')
+      .split('/')
+      .pop();
+      toast.error(errorMsg);
+    }
+  };
 
-  return <Login dispatch={dispatch} loginUser={loginUserWithEmail}/>
-}
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '70vh',
+      }}
+      className=""
+    >
+      <Box sx={{ width: { xs: '70%', sm: '50%', lg: '30%' }, m: '0 auto' }}>
+        <Login loginUser={loginUserWithEmail} />
+        <Link
+          sx={{ display: 'block', textAlign: 'center', textDecoration: 'none' }}
+          href="/registeruser"
+        >
+          Already have an account? Register
+        </Link>
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginUser;
