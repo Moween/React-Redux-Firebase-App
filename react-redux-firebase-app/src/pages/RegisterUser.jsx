c27eecb69c31f183a319f46c75dec81bd342afa9
moween <moween1998@gmail.com>
2022-01-19 23:18:48 +0100

    Implement signup feature

--- react-redux-firebase-app/src/pages/RegisterUser.jsx
+++ react-redux-firebase-app/src/pages/RegisterUser.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import Registration from '../utils/registerUser';
-import { useDispatch } from 'react-redux';
-import { useFirebase } from 'react-redux-firebase';
+import { useNavigate } from 'react-router-dom';
+import { firestoreConnect, useFirebase } from 'react-redux-firebase';
+import { toast } from 'react-toastify';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import { useFirestore } from 'react-redux-firebase';
+import { useSelector } from 'react-redux';
+
+import Registration from '../layout/form/RegisterUser';
 
 const RegisterUser = () => {
-  const dispatch = useDispatch();
-  const firebase = useFirebase();
+  const navigate = useNavigate();
+  const firestore = useFirestore();
+  const { createUser } = useFirebase();
+  const { uid } = useSelector((state) => state.firebase.auth);
 
   const createNewUser = async ({ email, password, username }) => {
-    await firebase.createUser({ email, password }, { username, email });
+    try {
+      await createUser({ email, password }, { username, email });
+      await firestore.collection('users').doc(uid);
+      toast.success('Signup successfull!!');
+      navigate('/', { replace: true});
+    } catch (error) {
+      const errorMsg = error.message
+        .replace(')', '')
+        .replace(/-/g, ' ')
+        .split('/')
+        .pop();
+      toast.error(errorMsg);
+    }
   };
 
-  return <Registration  dispatch={dispatch} registerUser={createNewUser}/>;
+  return (
+    <Box sx={{ width: { xs: '70%', sm: '50%', lg: '30%' }, m: '0 auto' }}>
+      <Registration registerUser={createNewUser} />
+      <Link
+        sx={{ display: 'block', textAlign: 'center', textDecoration: 'none' }}
+        href="/loginuser"
+      >
+        Already have an account? Log in
+      </Link>
+    </Box>
+  );
 };
 
 export default RegisterUser;
